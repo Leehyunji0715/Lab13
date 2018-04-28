ca27b74b9e4670baea4bddf95b8e52460020da34
leehyunji0715@gmail.com <leehyunji0715@172.17.220.87>
2018-04-28 17:24:23 +0900

    Circle-1

--- src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
@@ -1,5 +1,20 @@
 package edu.handong.csee.java.lab13.prob3;
+import java.util.Scanner;
+public class Circle extends ShapeInfo{
+	Scanner input = new Scanner(System.in);
+	double mRadius;
+	@Override
+	double area() {
+		return mRadius*mRadius*Math.PI;
+	}
+	@Override
+	double perimeter() {
+		return 2*Math.PI*mRadius;
+	}
 
-public class Circle {
+	void askInfo() {
+		System.out.println("Enter radius : ");
+		mRadius = input.nextInt();
+	}
 
 }
