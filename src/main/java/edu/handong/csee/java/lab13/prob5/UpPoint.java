4b573bed219a32d9b68a1b314f693bef026a11f5
leehyunji0715@gmail.com <leehyunji0715@172.17.156.101>
2018-04-29 15:47:11 +0900

    prob5 complete!

--- src/main/java/edu/handong/csee/java/lab13/prob5/UpPoint.java
+++ src/main/java/edu/handong/csee/java/lab13/prob5/UpPoint.java
@@ -3,8 +3,8 @@ package edu.handong.csee.java.lab13.prob5;
 public class UpPoint extends Printer {
 	int mX, mY;
 	public UpPoint(int x, int y) {
-		super.mX = x;
-		super.mY = y;
+		this.mX = x;
+		this.mY = y;
 	}
 	public String toString() {
 		return "X: "+mX+" Y : " +mY;
